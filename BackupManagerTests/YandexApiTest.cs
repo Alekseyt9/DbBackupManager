9c93cac07f9d1fc9ebe0b0c5ee4ce6b53a705dbc
Alekseyt9 <alekseyt9@gmail.com>
2023-07-05 19:59:56 +0300

    +

---
+++ BackupManagerTests/YandexApiTest.cs
@@ -0,0 +1,36 @@
+﻿
+using Xunit;
+using YandexDisk.Client;
+using YandexDisk.Client.Clients;
+using YandexDisk.Client.Http;
+using YandexDisk.Client.Protocol;
+
+namespace BackupManagerTests
+{
+    public class YandexApiTest
+    {
+        [Fact]
+        public async Task Test()
+        {
+            var dir = "/backups/test1/";
+
+            var oauthToken = "y0_AgAAAAALO2EBAAoi3wAAAADnCMTWyUDoupLASx2guxg8GITw4V8EA0s";
+            var diskApi = new DiskHttpApi(oauthToken);
+
+            try
+            {
+                var dirInfo = await diskApi.MetaInfo.GetInfoAsync(new ResourceRequest() { Path = dir });
+            }
+            catch (YandexApiException ex)
+            {
+                await diskApi.Commands.CreateDictionaryAsync(dir);
+            }
+
+            await diskApi.Files.UploadFileAsync(path: $"{dir}test111.txt",
+                overwrite: true,
+                localFile: @"..\..\..\data\test111.txt",
+                cancellationToken: CancellationToken.None);
+        }
+
+    }
+}
