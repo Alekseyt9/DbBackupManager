e3117c5ae6f98e6434c469c23996546ab2290116
Alekseyt9 <alekseyt9@gmail.com>
2023-07-08 15:14:36 +0300

    +

--- BackupManager/Common/TaskManager.cs
+++ BackupManager/Common/TaskManager.cs
@@ -17,37 +17,44 @@ namespace BackupManager
 
         private async Task RunTasks()
         {
-            _logger.LogMessage("1");
-            using var reader = new StreamReader("appsettings.json");
-            var str = reader.ReadToEnd();
-            var tasksModel = JsonConvert.DeserializeObject<TasksModel>(str);
-            _logger.LogMessage("2");
-
-            var properties = new NameValueCollection();
-            var scheduler = await SchedulerBuilder.Create(properties).BuildScheduler();
-
-            _logger.LogMessage($"Task count: {tasksModel.Tasks.Count}");
-
-            var i = 0;
-            foreach (var task in tasksModel.Tasks)
+            try
             {
-                var period = task.Schedule.Period;
+                _logger.LogMessage("1");
+                using var reader = new StreamReader("appsettings.json");
+                var str = await reader.ReadToEndAsync();
+                var tasksModel = JsonConvert.DeserializeObject<TasksModel>(str);
+                _logger.LogMessage("2");
 
-                var job = JobBuilder.Create<Job>()
-                    .WithIdentity($"job{i}", "group1")
-                    .UsingJobData("params", JsonConvert.SerializeObject(task))
-                    .Build();
+                var properties = new NameValueCollection();
+                var scheduler = await SchedulerBuilder.Create(properties).BuildScheduler();
 
-                var trigger = TriggerBuilder.Create()
-                    .WithIdentity($"trigger{i}", "group1")
-                    .StartNow()
-                    .WithCronSchedule(period)
-                    .Build();
+                _logger.LogMessage($"Task count: {tasksModel.Tasks.Count}");
 
-                await scheduler.ScheduleJob(job, trigger);
+                var i = 0;
+                foreach (var task in tasksModel.Tasks)
+                {
+                    var period = task.Schedule.Period;
+
+                    var job = JobBuilder.Create<Job>()
+                        .WithIdentity($"job{i}", "group1")
+                        .UsingJobData("params", JsonConvert.SerializeObject(task))
+                        .Build();
+
+                    var trigger = TriggerBuilder.Create()
+                        .WithIdentity($"trigger{i}", "group1")
+                        .StartNow()
+                        .WithCronSchedule(period)
+                        .Build();
+
+                    await scheduler.ScheduleJob(job, trigger);
+                }
+
+                await scheduler.Start();
             }
-
-            await scheduler.Start();
+            catch (Exception ex)
+            {
+                _logger.LogError(ex.Message);
+            }
         }
 
 
